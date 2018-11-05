c4b6461813c4b6276a09e9a91449fdfbea0442d2
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-11-05 12:23:47 +0100

    Started building basic Events controller

---
+++ src/Bileteria.Api/Controllers/EventsController.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Bileteria.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bileteria.Api.Controllers
+{
+    [Route("[controller]")]
+    public class EventsController : Controller
+    {
+        private readonly IEventService _eventService;
+        public EventsController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(string name)
+        {
+            var events = await _eventService.BrowseAsync(name);
+
+            return Json(events);
+        }
+    }
+}
