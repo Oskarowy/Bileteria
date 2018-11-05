1ae1cf8024202a05aa27d02ca1eaf185bef9c25a
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-11-05 12:10:51 +0100

    Configured AutoMapper; started implementing of Event Service

---
+++ src/Bileteria.Infrastructure/Mappers/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+using AutoMapper;
+using Bileteria.Core.Domain;
+using Bileteria.Infrastructure.DTO;
+
+namespace Bileteria.Infrastructure.Mappers
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Initialize()
+            => new MapperConfiguration(cfg => 
+            {
+                cfg.CreateMap<Event, EventDto>()
+                    .ForMember(x => x.TicketsCount, m => m.MapFrom(p => p.Tickets.Count()));
+            })
+            .CreateMapper();
+    }
+}
