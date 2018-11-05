62aca41e83b3c197a0ee4b27de144ce00d4e4b89
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-11-05 12:17:54 +0100

    Configured Startup.cs for dependency injection

--- src/Bileteria.Api/Startup.cs
+++ src/Bileteria.Api/Startup.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bileteria.Core.Repositories;
+using Bileteria.Infrastructure.Mappers;
+using Bileteria.Infrastructure.Repositories;
+using Bileteria.Infrastructure.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +30,10 @@ namespace Bileteria.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IEventService, EventService>();
+            services.AddSingleton(AutoMapperConfig.Initialize());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
