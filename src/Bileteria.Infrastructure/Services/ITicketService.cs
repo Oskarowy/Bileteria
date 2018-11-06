62b3fa0ba9065e646e49967684007b06ed2e947d
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-11-06 08:29:53 +0100

    Created ticket service interface

---
+++ src/Bileteria.Infrastructure/Services/ITicketService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading.Tasks;
+using Bileteria.Infrastructure.DTO;
+
+namespace Bileteria.Infrastructure.Services
+{
+    public interface ITicketService
+    {
+        Task<TicketDto> GetAsync(Guid userId, Guid eventId, Guid ticketId);
+        Task PurchaseAsync(Guid userId, Guid eventId, int amount);
+        Task CancelAsync(Guid userId, Guid eventId, int amount);
+    }
+}
